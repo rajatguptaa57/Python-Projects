2b6335e52de7f371fc490f4368c0b2ad489b9063
Rajat Gupta <rajatgupta1798@gmail.com>
2020-05-26 11:31:21 +0530

    Update PDF.py

--- PDF.py
+++ PDF.py
@@ -1,28 +1,21 @@
+# Combining 2 given PDFs using PyPDF2
+
 import PyPDF2
 
-pdfFile = open('meetingminutes1.pdf', 'rb')
-
-reader = PyPDF2.PdfFileReader(pdfFile)
-
-print(reader.numPages)
-
-page = reader.getPage(0)
-
-print(page.extractText())
-
-for pageNum in range(reader.numPages):
-    print(reader.getPage(pageNum).extractText())
-
+# Open both PDFs
 pdf1File = open('meetingminutes1.pdf', 'rb')
 
 pdf2File = open('meetingminutes2.pdf', 'rb')
 
+# Create object for reading 
 reader1 = PyPDF2.PdfFileReader(pdf1File)
 
 reader2 = PyPDF2.PdfFileReader(pdf2File)
 
+# Create object for writing
 writer = PyPDF2.PdfFileWriter()
 
+# Read and write contents of PDF into writer object
 for pageNum in range(reader1.numPages):
     page = reader1.getPage(pageNum)
     writer.addPage(page)
@@ -31,6 +24,7 @@ for pageNum in range(reader2.numPages):
     page = reader2.getPage(pageNum)
     writer.addPage(page)
 
+# Write into a new PDF    
 outputFile = open('combinedminutes.pdf', 'wb')
 writer.write(outputFile)
 outputFile.close()
